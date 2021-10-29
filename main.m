0ae65ad304c6c542dca0798697ba7cbb62740004
smano32 <maniscst@bc.edu>
2021-10-29 16:00:00 -0400

    Main should now read in data, identify mins and maxes, and plot them. Issue with MER data, dates go back in time after price hits about 93.

--- main.m
+++ main.m
@@ -1,8 +1,91 @@
+%% Read in Data
 pfe = excelReader("PFE");
-[maxima, maxIdx, minima, minIdx] = maxes_mins(pfe);
+mer = excelReader("MER"); % issue with mer data, dates overlap after price drops from 90
+aes = excelReader("AES");
+aal = excelReader("AAL");
+
+%% Calculate Mins and Maxes
+pfeDate = pfe(:,1);
+pfePrice = pfe(:,2);
+[maximaPfe, maxIdxPfe, minimaPfe, minIdxPfe] = maxes_mins(pfePrice);
+pfeMaxDate = [];
+for i = (1:length(maxIdxPfe))
+    pfeMaxDate(i) = pfeDate(maxIdxPfe(i));
+end
+pfeMinDate = [];
+for i = (1:length(minIdxPfe))
+    pfeMinDate(i) = pfeDate(minIdxPfe(i));
+end
+
+merDate = mer(:,1);
+merPrice = mer(:,2);
+[maximaMer, maxIdxMer, minimaMer, minIdxMer] = maxes_mins(merPrice);
+merMaxDate = [];
+for i = (1:length(maxIdxMer))
+    merMaxDate(i) = merDate(maxIdxMer(i));
+end
+merMinDate = [];
+for i = (1:length(minIdxMer))
+    merMinDate(i) = merDate(minIdxMer(i));
+end
+
+aesDate = aes(:,1);
+aesPrice = aes(:,2);
+[maximaAes, maxIdxAes, minimaAes, minIdxAes] = maxes_mins(aesPrice);
+aesMaxDate = [];
+for i = (1:length(maxIdxAes))
+    aesMaxDate(i) = aesDate(maxIdxAes(i));
+end
+aesMinDate = [];
+for i = (1:length(minIdxAes))
+    aesMinDate(i) = aesDate(minIdxAes(i));
+end
+
+aalDate = aal(:,1);
+aalPrice = aal(:,2);
+[maximaAal, maxIdxAal, minimaAal, minIdxAal] = maxes_mins(aalPrice);
+aalMaxDate = [];
+for i = (1:length(maxIdxAal))
+    aalMaxDate(i) = aalDate(maxIdxAal(i));
+end
+aalMinDate = [];
+for i = (1:length(minIdxAal))
+    aalMinDate(i) = aalDate(minIdxAal(i));
+end
+
+%% Plot Data
+figure
+plot(pfeDate,pfePrice)
+hold on
+plot(pfeMaxDate,maximaPfe,'og')
+plot(pfeMinDate,minimaPfe,'or')
+title('PFE')
+xlabel('Date')
+ylabel('Price')
 
 figure
-plot(data)
+plot(merDate,merPrice)
 hold on
-plot(maxIdx,maxima,'og')
-plot(minIdx,minima,'or')
+plot(merMaxDate,maximaMer,'og')
+plot(merMinDate,minimaMer,'or')
+title('MER')
+xlabel('Date')
+ylabel('Price')
+
+figure
+plot(aesDate,aesPrice)
+hold on
+plot(aesMaxDate,maximaAes,'og')
+plot(aesMinDate,minimaAes,'or')
+title('AES')
+xlabel('Date')
+ylabel('Price')
+
+figure
+plot(aalDate,aalPrice)
+hold on
+plot(aalMaxDate,maximaAal,'og')
+plot(aalMinDate,minimaAal,'or')
+title('AAL')
+xlabel('Date')
+ylabel('Price')
