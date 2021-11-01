eddc875fb6bb840f14b99e4fe277e5959ef5e682
Cole <cadumas01@gmail.com>
2021-10-31 22:21:26 -0400

    movingAvgs extrema and first linear regression

--- analyze.m
+++ analyze.m
@@ -5,37 +5,47 @@ function analyze(fileName)
     %% Read in Data
     data = excelReader(fileName);
    
-    %% Calculate Mins and Maxes
+    %% Calculate Mins and Maxes of Price
     date = data(:,1);
     price = data(:,2);
-    [maxima, maxIdx, minima, minIdx] = maxes_mins(price);
+    [maxima, maxDate, minima, minDate] = maxes_mins(date, price);
+  
     
-    maxDate = alignByDate(date,maxIdx);
-    minDate = alignByDate(date,minIdx);
-   
-    %% Setting up Moving Averages
+    %% Calculate Moving Average
     %k pt. movinge means
 
-    k = fix(length(data)/25);    
-    movingAvg = movmean(price,k);
+    k = fix(length(data)/20);    
+    movAvg = movmean(price,k);
 
+    
+    %% Caluclate Mins and Max of Moving Avg 
+    [maximaMovAvg, maxMovAvgDate, minimaMovAvg, minMovAvgDate] = maxes_mins(date,movAvg);
+    
+    %% Running Regressions
+    regMovAvgMax1 = fitlm(maxMovAvgDate,maximaMovAvg);
+    
     %% Plot Data
     figure
     plot(date,price)
     hold on
-    plot(maxDate,maxima,'og')
-    plot(minDate,minima,'or')
-    plot(date,movingAvg,'p')
+   % plot(maxDate,maxima,'og') % disabled low scale extrema for now
+    %plot(minDate,minima,'or') % disabled 
+    
+    plot(date,movAvg,'m')
+    
+    plot(maxMovAvgDate,maximaMovAvg,'^g')
+    plot(regMovAvgMax1);
+    
+    
+    plot(minMovAvgDate,minimaMovAvg,'^r')
+    
     title(fileName)
     xlabel('Date')
     ylabel('Price')
 end
 
-%Aligns matrix m with Dates of m's parent matrix
-function [mDate] = alignByDate(date,mIdx)
-    len = length(mIdx);
-    mDate = zeros(len,1);
-    for i = (1:len)
-        mDate(i) = date(mIdx(i));
-    end
+% If the new floor is equal to the old ceiling (within a interval),
+% Then cut the data into two sections
+function [x,y] = floorCeilingCut(x,y)
+%Add code here
 end
--- maxes_mins.m
+++ maxes_mins.m
@@ -1,9 +1,21 @@
-function [maxima, maxIdx, minima, minIdx] = maxes_mins(data)
+function [maxima, maxDate, minima, minDate] = maxes_mins(x,y)
 
-%% Find Local Mins and Maxes
-% run to plot local mins and maxes on stock plot
-[maxima,maxIdx] = findpeaks(data);
-[minima,minIdx] = findpeaks(-data);
-minima = minima * -1;
+    %% Find Local Mins and Maxes
+    % run to plot local mins and maxes on stock plot
+    [maxima,maxIdx] = findpeaks(y);
+    [minima,minIdx] = findpeaks(-y);
+    minima = minima * -1;
+
+    maxDate = alignByX(x,maxIdx);
+    minDate = alignByX(x,minIdx);
+
+    %Aligns matrix m with Dates of m's parent matrix
+    function [mDate] = alignByX(x,mIdx)
+        len = length(mIdx);
+        mDate = zeros(len,1);
+        for i = (1:len)
+            mDate(i) = x(mIdx(i));
+        end
+    end
 
 end
