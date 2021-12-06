9b94d7984deb56422cd3cd2a361a3fd51b16bd2b
John <clarkpw@bc.edu>
2021-12-05 21:01:16 -0500

    Updated syntax

--- Trends.m
+++ Trends.m
@@ -3,13 +3,13 @@ function [rtrend strend linem, linemi] = trends(dates,maxd,mind, maxp, minp)
    e = dates(2);
    maxd = maxd';
    mind = mind';
-   [val,i]=min(abs(maxd-s));
+   [v,i]=min(abs(maxd-s));
    mas = i;
-   [val,i]=min(abs(maxd-e));
+   [v,i]=min(abs(maxd-e));
    mae = i;
-   [val,i]=min(abs(mind-s));
+   [v,i]=min(abs(mind-s));
    mis = i;
-   [val,i]=min(abs(mind-e));
+   [v,i]=min(abs(mind-e));
    mie = i;
    fprintf("\n Here is mie: %i | here is mis: %i ", mie, mis)
    strend = (minp(mie)-minp(mis))/(mind(mie)-mind(mis));
